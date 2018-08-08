469683bf521603efa12cd464a7696e936f7d7819
Miguel-Parra <miguelts644@hotmail.com>
2018-08-08 08:23:46 -0500

    a

--- Proyecto_Final/MostrarP.php
+++ Proyecto_Final/MostrarP.php
@@ -128,7 +128,7 @@
                 <tr valign="middle" align="center">
                     <td><a href="ProductoM.php?idProducto=<?php echo $row['IDPRODUCTO']; ?>"><span class="glyphicon glyphicon-pencil"></span></a>      |
                         <a href="EliminarP.php?idProducto=<?php echo $row['IDPRODUCTO']; ?>"><span class="glyphicon glyphicon-trash"></span></a>
-                        <a href="Producto/MostrarEtiqueta.php?idProducto=<?php echo $row['IDPRODUCTO']; ?>"><span class="btn btn-primary">Mostrar Etiqueta</span></a>
+                        <a href="MostrarEtiquetaEmpresa.php?idProducto=<?php echo $row['IDPRODUCTO']; ?>"><span class="btn btn-primary">Mostrar Etiqueta</span></a>
 
                     </td>
                     <td><?php echo $row['IDPRODUCTO']; ?></td>
