8d493be59d3dd8e274193ea225ede88a4efe77ed
Miguel-Parra <miguelts644@hotmail.com>
2018-08-07 23:23:37 -0500

    Arreglale Ewin

--- Proyecto_Final/MostrarEtiquetaProduc.php
+++ Proyecto_Final/MostrarEtiquetaProduc.php
@@ -1,6 +1,12 @@
 <html lang="es">
 <head>
     <!-- Bootstrap core CSS -->
+    <meta name="viewport" content="width=device-width, initial-scale=1"> <!--detecta un dispositivo para adaptar la pantalla-->
+    <link href="Css/bootstrap.min.css" rel="stylesheet"> <!--//direccion de hojas de estilo de bootstrap-->
+    <link href="Css/bootstrap-theme.css" rel="stylesheet"> <!--//direccion de hojas de estilo de bootstrap-->
+    <link href="Css/estiloScrollTable.css" rel="stylesheet">
+    <script src="js/jquery-3.1.1.min.js"></script> <!--javascript-->
+    <script src="js/bootstrap.min.js"></script>
     <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link href="Css/estiloMostrarEtiquetaProducto.css" rel="stylesheet">
@@ -17,18 +23,9 @@ while($row1 = mysqli_fetch_array($resultado,MYSQLI_ASSOC)) {
     $variableSODIO = $row1['SODIOS'];
 }
 
-$sql1 = "SELECT * FROM ETIQUETANUTRICIONAL WHERE IDPRODUCTO ='$idPRODUCTO'";
-$resultado1 = $mysqli->query($sql1);
+    $sql1 = "SELECT * FROM ETIQUETANUTRICIONAL WHERE IDPRODUCTO ='$idPRODUCTO'";
 
-while($row = mysqli_fetch_array($resultado,MYSQLI_ASSOC)) {
-    echo $row['IDPRODUCTO'];
-    echo $row['ENERGIAT'];
-    echo $row['GRASAT'];
-    echo $row['COLESTEROLT'];
-    echo $row['SODIOT'];
-    echo $row['CARBOHIDRATOT'];
-    echo $row['PROTEINAT'];
-}
+    $resultado1 = $mysqli->query($sql1);
 ?>
 </head>
 
@@ -117,6 +114,42 @@ while($row = mysqli_fetch_array($resultado,MYSQLI_ASSOC)) {
 <div class="col-md-6">.col-md-6</div>
 
 </div>
+
+
+
+<!-- ETIQUETA
+    ================================================== -->
+
+<div class="container" align="center"> <!--agregar un contenedor-->
+    <div class="row"> <!--nueva fila-->
+        <h2 align="center">ETIQUETA NUTRICIONAL </h2> <!--encabezado-->
+    </div>
+    <br><br>
+
+    <div class="row table-striped">
+        <table class="table table-striped table-bordered text-center" style="width: 50% " border="5px solid">
+            <!-- cabecera -->
+            <?php while($row = mysqli_fetch_array($resultado1,MYSQLI_ASSOC)) { ?>
+
+                <tr><th> Energía Total</th><td><?php echo $row['ENERGIAT']; ?> g </td></tr>
+                <tr><th> Grasa Total</th><td><?php echo $row['GRASAT']; ?> g </td></tr>
+                <tr><th> Colesterol</th><td><?php echo $row['COLESTEROLT']; ?> mg </td></tr>
+                <tr><th> Sodio</th><td><?php echo $row['SODIOT']; ?> g </td></tr>
+                <tr><th> Carbohidratos Totales</th><td><?php echo $row['CARBOHIDRATOT']; ?> g </td></tr>
+                <tr><th> Proteínas</th><td><?php echo $row['PROTEINAT']; ?> g </td></tr>
+
+            <?php } ?>
+
+        </table>
+
+    </div>
+
+    <br>
+    <div style="text-align: left;width:220px">
+        <a href="MostrarPCliente.php" class="btn btn-primary" >Retornar</a>
+    </div>
+</div>
+
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
